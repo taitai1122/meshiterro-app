d205dc74dcb01dc5038761e83b3c5482c548e774
taitai1122 <taikicody592@icloud.com>
2021-09-14 16:50:29 +0000

    Initial commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '7623b2f97531486f8a5c228c8c8c37569343f239de640e6324cd703c585cf01a0e0a305b98a634cc0ae185502b22fb8e3043b27e60ced2ce66840ef0782c5535'
+
---
+++ db/migrate/20210914160337_ado_profile_imageld_to_users.rb
@@ -0,0 +1,4 @@
+class AdoProfileImageldToUsers < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
